504805a85bdd3d83dfda2f6ab4c490d0af0eca71
Leonardo Augusto <leo.augustobh13@gmail.com>
2022-03-28 16:36:51 -0300

    create function erro

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 import chalk from 'chalk';
 import * as fs from 'fs';
 
+function trataErro(erro) {
+    throw new Error(chalk.red(erro));
+}
+
 
 function pegaArquivo(caminhoDoArquivo) {
 
     const enconding = 'utf8';
-    fs.readFile(caminhoDoArquivo, enconding, (_, texto) => {
+    fs.readFile(caminhoDoArquivo, enconding, (erro, texto) => {
+        if (erro) {
+            trataErro(erro);
+        }
         console.log(chalk.green(texto));
-    })  
+    })
 }
 
 pegaArquivo('./arquivos/texto1.md');
